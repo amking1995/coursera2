e4654186a25c7397a2907518ddd5ae19bcea8ed2
amking1995 <68598576+amking1995@users.noreply.github.com>
2020-07-31 10:01:43 -0400

    Update coursera_hw.r

--- coursera_hw.r
+++ coursera_hw.r
@@ -17,7 +17,8 @@ inv <- NULL
   getInverse = getInverse)
 }
 
-# With the function that we have created, it will compute the inverse of the special "matrix" returned by the function from above. If we have calculated the inverse, then the cachesolve should get the inverse. We can then compute the inverse of the square matrix using R.
+# With the function that we have created, it will compute the inverse of the special "matrix" returned by the function from above. 
+# If we have calculated the inverse, then the cachesolve should get the inverse. We can then compute the inverse of the square matrix using R.
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of x
